882a8057ebc0d813fc2aa312c8b601cfa2f64205
Tran Duc Hai <haiductran712@gmail.com>
2025-12-19 21:39:44 +0700

    uc d1-d2-d3-d4

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const authRoutes = require("./auth.routes");
 const userRoutes = require("./user.routes");
+const drugRoutes = require("./drug.routes");
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
+router.use(drugRoutes);
 
 module.exports = router;
