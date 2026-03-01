88e76b93332ba7232545b933bc98a3646cecccc3
Tran Duc Hai <haiductran712@gmail.com>
2026-03-01 16:55:38 +0700

    1

--- src/config/env.js
+++ src/config/env.js
@@ -35,6 +35,19 @@ const config = {
     fromName: process.env.EMAIL_FROM_NAME || "CareDose",
     fromAddress: process.env.EMAIL_FROM_ADDRESS || process.env.EMAIL_USER || "",
   },
+  firebase: {
+    type: process.env.FB_TYPE,
+    project_id: process.env.FB_PROJECT_ID,
+    private_key_id: process.env.FB_PRIVATE_KEY_ID,
+    private_key: process.env.FB_PRIVATE_KEY,
+    client_email: process.env.FB_CLIENT_EMAIL,
+    client_id: process.env.FB_CLIENT_ID,
+    auth_uri: process.env.FB_AUTH_URI,
+    token_uri: process.env.FB_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FB_AUTH_PROVIDER,
+    client_x509_cert_url: process.env.FB_CLIENT_X500,
+    universe_domain: process.env.FB_UNIVERSE_DOMAIN,
+  },
 };
 
 module.exports = config;
